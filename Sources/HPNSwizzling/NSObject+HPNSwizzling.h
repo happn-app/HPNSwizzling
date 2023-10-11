66df1dd0892b79f6844ea5c7be13542d2d25bec5
Frizlab <fload@me.com>
2023-10-11 20:30:15 +0300

    Change some comments

--- Sources/HPNSwizzling/NSObject+HPNSwizzling.h
+++ Sources/HPNSwizzling/NSObject+HPNSwizzling.h
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License. */
 
 /* Thank you <https://stackoverflow.com/a/8636521>.
- * I had to enhance though (`+load` of categories is not always called after `+load` of superclass’s categories). */
+ * I had to enhance though (`+load` of categories is not always called after `+load` of superclass’s categories).
+ *
+ * ⚠️ None of these methods are thread-safe! */
 
 #import <Foundation/Foundation.h>
 
@@ -27,8 +29,8 @@ limitations under the License. */
 typedef IMP *IMPPointer;
 
 
+/* Note: We have beautiful documentation comments… that are fully ignored by DocC, and are incorrectly parsed by Xcode ¯\_(ツ)_/¯ */
 @interface NSObject (Swizzling)
-/* WARNING: None of these methods are thread-safe! */
 
 /* How to method-swizzle?
  * Explanation by example below.
@@ -70,7 +72,7 @@ static void MySetFrame(id self, SEL _cmd, CGRect frame) {
 /**
  Tries to swizzle the original selector with the given replacement, setting `store` to the original implementation.
  On success, returns `YES`, and set `*didAddPtr` to `NO`.
- If the original selector does not exist in the class, will try to add the implementation to the class using the `typesSelector` if it is not `NULL`. 
+ If the original selector does not exist in the class, will try to add the implementation to the class using the `typesSelector` if it is not `NULL`.
  On success, `*store` is set to `NULL`, `*didAddPtr` is set to `YES`.
  
  About `fixChildrenSwizzling`:
@@ -90,8 +92,7 @@ static void MySetFrame(id self, SEL _cmd, CGRect frame) {
  When a superclass swizzles a method a child swizzled, the store of the child will be updated to point to the new original implementation.
  This also works if there was no original implementation (store set to `NULL`) and a superclass adds one.
  
- - Note:
- If `store` is non-`NULL`, it must always point to a valid memory location while the application is launched (unless you don’t plan on fixing children swizzling).
+ - Note: If `store` is non-`NULL`, it must always point to a valid memory location while the application is launched (unless you don’t plan on fixing children swizzling).
  It is highly recommended that you keep a reference to the original implementation of the method using the store variable:
   when you swizzle a method, you practically always have to call the original method.
  (Giving a `NULL` `store` will print a warning in the logs.) */
@@ -117,8 +118,7 @@ static void MySetFrame(id self, SEL _cmd, CGRect frame) {
  
  If this method returns `YES`, `*store` will always be `NULL` when going out of the method.
  
- - Note:
- If store is non-`NULL`, it must always point to a valid memory location while the application is launched
+ - Note: If store is non-`NULL`, it must always point to a valid memory location while the application is launched
   (unless you don’t plan on fixing children swizzling).
  (This is the same rule as the `hpn_swizzleOrAdd:...` methods.) */
 + (BOOL)hpn_addOnlyIfNotExist:(SEL)added with:(IMP)implementation typesSelector:(SEL)typesSelector store:(IMPPointer)store;
